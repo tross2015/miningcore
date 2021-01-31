b55fb928cd14a4e44906a3b7a32e2bdf779db511
AlphaX-Projects <77661270+AlphaX-Projects@users.noreply.github.com>
2021-01-31 14:23:17 +0100

    fix poolhash

--- src/Miningcore/Mining/StatsRecorder.cs
+++ src/Miningcore/Mining/StatsRecorder.cs
@@ -181,7 +181,7 @@ namespace Miningcore.Mining
                         var poolHashesCountAccumulated = result.Sum(x => x.Count);
                         var poolHashrate = pool.HashrateFromShares(poolHashesAccumulated, windowActual) * HashrateBoostFactor;
                         
-                        if(poolId == "idx" || poolId == "vgc" || poolId == "shroud" || poolId == "ecc"){
+                        if(poolId == "idx" || poolId == "vgc" || poolId == "shroud" || poolId == "ecc" || poolId == "gold"){
                             poolHashrate *= 11.2;
                         }
                         
@@ -262,7 +262,7 @@ namespace Miningcore.Mining
                             if(windowActual >= MinHashrateCalculationWindow)
                             {
                                 var hashrate = pool.HashrateFromShares(item.Sum, windowActual) * HashrateBoostFactor;
-                                if(poolId == "idx" || poolId == "vgc" || poolId == "shroud" || poolId == "ecc"){
+                                if(poolId == "idx" || poolId == "vgc" || poolId == "shroud" || poolId == "ecc" || poolId == "gold"){
                                     hashrate *= 11.2;
                                 }
                                 minerTotalHashrate += hashrate;
