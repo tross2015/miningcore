55e9004fb9fc5ce3b29f21177eb60761b2cad808
AlphaX-Projects <77661270+AlphaX-Projects@users.noreply.github.com>
2021-02-11 11:29:22 +0100

    Renew ETH

--- src/Miningcore/Blockchain/Ethereum/DaemonResponses/GetBlockResponse.cs
+++ src/Miningcore/Blockchain/Ethereum/DaemonResponses/GetBlockResponse.cs
@@ -1,6 +1,7 @@
 /*
 Copyright 2017 Coin Foundry (coinfoundry.org)
 Authors: Oliver Weichhold (oliver@weichhold.com)
+         Olaf Wasilewski (olaf.wasilewski@gmx.de)
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
 associated documentation files (the "Software"), to deal in the Software without restriction,
