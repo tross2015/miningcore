08d60af7887073d33fccc2f7d929d1d4b7f7dc1f
AlphaX-Projects <77661270+AlphaX-Projects@users.noreply.github.com>
2021-02-15 17:44:05 +0100

    Update blake2s.c

--- src/Native/libmultihash/blake2s.c
+++ src/Native/libmultihash/blake2s.c
@@ -6,10 +6,12 @@
 
 #include "sha3/sph_blake2s.h"
 
-void blake2s_hash(const char* input, char* output, int inlen)
+void blake2s_hash(const char* input, char* output, uint32_t len)
 {
-    blake2s_state ctx_blake2s;
-    blake2s_init(&ctx_blake2s, BLAKE2S_OUTBYTES);
-    blake2s_update(&ctx_blake2s, input, inlen);
-    blake2s_final(&ctx_blake2s, output, BLAKE2S_OUTBYTES);
+	uint8_t hash[BLAKE2S_OUTBYTES];
+	blake2s_state blake2_ctx;
+	blake2s_init(&blake2_ctx, BLAKE2S_OUTBYTES);
+	blake2s_update(&blake2_ctx, input, len);
+	blake2s_final(&blake2_ctx, hash, BLAKE2S_OUTBYTES);
+	memcpy(output, hash, 32);
 }
