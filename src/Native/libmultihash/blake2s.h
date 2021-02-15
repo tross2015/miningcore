5a313df0c0c6324da7c0c0283acbf3e1e8125c01
AlphaX-Projects <77661270+AlphaX-Projects@users.noreply.github.com>
2021-02-15 17:37:33 +0100

    Update blake2s.h

--- src/Native/libmultihash/blake2s.h
+++ src/Native/libmultihash/blake2s.h
@@ -7,7 +7,7 @@ extern "C" {
 
 #include <stdint.h>
 
-void blake2s_hash(const char* input, char* output, int inlen);
+void blake2s_hash(const char* input, char* output, uint32_t len);
 
 #ifdef __cplusplus
 }
