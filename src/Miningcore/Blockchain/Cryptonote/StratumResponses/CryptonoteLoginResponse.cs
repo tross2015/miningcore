e349a2cc6cdac69d2a378a8a1f0c4c2ed2b3a9c3
AlphaX-Projects <77661270+AlphaX-Projects@users.noreply.github.com>
2021-02-16 12:20:29 +0100

    add seedhash

--- src/Miningcore/Blockchain/Cryptonote/StratumResponses/CryptonoteLoginResponse.cs
+++ src/Miningcore/Blockchain/Cryptonote/StratumResponses/CryptonoteLoginResponse.cs
@@ -1,6 +1,7 @@
 /*
 Copyright 2017 Coin Foundry (coinfoundry.org)
 Authors: Oliver Weichhold (oliver@weichhold.com)
+         Olaf Wasilewski (olaf.wasilewski@gmx.de)
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
 associated documentation files (the "Software"), to deal in the Software without restriction,
@@ -29,6 +30,9 @@ namespace Miningcore.Blockchain.Cryptonote.StratumResponses
 
         public string Blob { get; set; }
         public string Target { get; set; }
+        
+        [JsonProperty("seed_hash")]
+        public string SeedHash { get; set; }
 
         /// <summary>
         /// Introduced for CNv4 (aka CryptonightR)
